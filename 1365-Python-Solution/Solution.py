de73b14004d9e7845f90ce008c0b34f559c54935
Yash Shah <yashah1432@gmail.com>
2026-08-12 11:18:43 +0530

    Add solution for LeetCode Python Problem 1365

---
+++ 1365-Python-Solution/Solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def smallerNumbersThanCurrent(self, nums):
+        result = []
+        for i in range(0, len(nums)):
+            count = 0
+            for j in range(0, len(nums)):
+                if nums[i] > nums[j]:
+                    count += 1
+            result.append(count)
+        return result
