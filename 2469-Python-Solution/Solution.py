c63cbbb44d8ee148f68985a9f9eda91ba3a875bf
Yash Shah <yashah1432@gmail.com>
2026-08-13 18:37:10 +0530

    Add solution for LeetCode Python Problem 2469

---
+++ 2469-Python-Solution/Solution.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def convertTemperature(self, celsius):
+        ans = []
+        ans.append(round(celsius + 273.15, 5))
+        ans.append(round(celsius * 1.80 + 32.00, 5))
+        return ans
