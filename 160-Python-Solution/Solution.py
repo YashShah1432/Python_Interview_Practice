5238dfab17e2229583571fd7b37f2f07e23eea69
Yash Shah <yashah1432@gmail.com>
2026-08-30 09:31:10 +0530

    Add solution for LeetCode Python Problem 160

---
+++ 160-Python-Solution/Solution.py
@@ -0,0 +1,37 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        p1 = headA
+        p2 = headB
+        lenA = 0
+        lenB = 0
+
+        while p1:
+            p1 = p1.next
+            lenA += 1
+
+        while p2:
+            p2 = p2.next
+            lenB += 1
+        
+        p1 = headA
+        p2 = headB
+        diff = abs(lenA - lenB)
+        if lenA > lenB:
+            for i in range(diff):
+                p1 = p1.next
+        else:
+            for i in range(diff):
+                p2 = p2.next
+        
+        while p1:
+            if p1 == p2:
+                return p1
+            p1 = p1.next
+            p2 = p2.next
+        return None
